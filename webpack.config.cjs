d7562629d8c652ac682c397d00b08e160297f251
polinagv <pol.grachewa@yandex.ru>
2024-02-25 20:18:17 +0300

    fix

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -25,10 +25,10 @@ const config = {
       //   test: /\.(js|jsx)$/i,
       //   loader: 'babel-loader',
       // },
-      {
-        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-        type: 'asset',
-      },
+      // {
+      //   test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+      //   type: 'asset',
+      // },
       {
         test: /\.js$/,
         exclude: /node_modules/,
